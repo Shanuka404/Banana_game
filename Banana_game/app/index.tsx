144b61b61725991b8e167ab30ede3e0cf83cb50e
Devinda <shanukadeva2000@gmail.com>
2024-11-07 07:52:13 -0800

    Fixed the errors in NativeWind setup

--- Banana_game/app/index.tsx
+++ Banana_game/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native'
 
 const index = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-">Banana Game</Text>
+    <View className="flex-1 items-center justify-center bg-green-500">
+      <Text className="text-red-700 text-3xl">Banana Game</Text>
     </View>
   )
 }
